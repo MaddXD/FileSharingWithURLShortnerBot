136038fb0f13411870f944e2de0335e1fc234d8a
Mahesh Gogi <154555616+mgbott@users.noreply.github.com>
2024-02-07 23:28:26 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "25537043"))
+  API_HASH = os.environ.get("API_HASH", "513962d838d498ca36461d6f9de2e66b")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6949256418:AAHmfgVxgbbR9xicO48enMBSrpTJHDqmunI")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "SipiccpBot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002002408186"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "publicearn.com")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "c2208684d7ebad60b2b12a48dd9cbab82f4f33db")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6816465945"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mgworks24:jhUVcpZ0Uiw8Svhd@cluster0.rj2bxk0.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002055755406")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002113782068"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -23,27 +23,12 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/SipiccpBot)
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
 ├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
 │
 ╰──────[ 😎 ]───────────⍟
-"""
-  ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
- 
- I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
-"""
-  HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
-
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
-"""
+  
